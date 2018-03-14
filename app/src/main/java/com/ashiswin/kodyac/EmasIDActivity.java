629c7085a88d78ceac178e6efd2fbbcc68302f64
limJingYun <limjingyun97@gmail.com>
2018-03-14 14:10:46 +0800

    ImageListener (try)
    trying to get ImageListener to work
    
    General (Hacky) Plan:
    make Uri from file
    read the URi from the string and if it is not null set it to the Image View
    
    *retrieve image from Uri also chase vincent to collaborate

--- app/src/main/java/com/ashiswin/kodyac/EmasIDActivity.java
+++ app/src/main/java/com/ashiswin/kodyac/EmasIDActivity.java
@@ -7,19 +7,22 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
+import com.microblink.image.Image;
+import com.microblink.image.ImageListener;
+import com.microblink.metadata.MetadataSettings;
 import com.microblink.recognizers.blinkid.singapore.combined.SingaporeIDCombinedRecognitionResult;
 import com.microblink.recognizers.blinkid.singapore.combined.SingaporeIDCombinedRecognizerSettings;
 import com.microblink.activity.VerificationFlowActivity;
@@ -27,7 +30,12 @@ import com.microblink.results.date.Date;
 import com.microblink.util.RecognizerCompatibility;
 import com.microblink.util.RecognizerCompatibilityStatus;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 
 //Tutorial: https://github.com/BlinkID/blinkid-android#quickDemo
@@ -78,12 +86,23 @@ public class EmasIDActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 SingaporeIDCombinedRecognizerSettings settings = new SingaporeIDCombinedRecognizerSettings();
+                //so i can extract the face image
                 settings.setEncodeFaceImage(true);
+
+                MetadataSettings.ImageMetadataSettings ims = new MetadataSettings.ImageMetadataSettings();
+                // enable obtaining of dewarped(cropped) images
+                ims.setDewarpedImageEnabled(true);
+                // obtain successful frames (full last frame on which scan has succeeded)
+                // if you want to obtain only dewarped(cropped) images do not enable successful scan frames
+                ims.setSuccessfulScanFrameEnabled(true);
+
                 profilePictest=settings.shouldEncodeFaceImage();
                 Intent intent = new Intent(EmasIDActivity.this, VerificationFlowActivity.class);
                 intent.putExtra(VerificationFlowActivity.EXTRAS_LICENSE_KEY, getString(R.string.microblink_license_key));
                 intent.putExtra(VerificationFlowActivity.EXTRAS_COMBINED_RECOGNIZER_SETTINGS, settings);
                 intent.putExtra(VerificationFlowActivity.EXTRAS_BEEP_RESOURCE, R.raw.beep);
+                intent.putExtra(VerificationFlowActivity.EXTRAS_IMAGE_LISTENER, new MyImageListener());
+                intent.putExtra(VerificationFlowActivity.EXTRAS_IMAGE_METADATA_SETTINGS,ims);
                 //TODO: sort out the hints so the UI is better
                 intent.putExtra(VerificationFlowActivity.EXTRAS_INSTRUCTIONS_DOCUMENT_FIRST_SIDE, R.string.emas_scan_first_side);
                 intent.putExtra(VerificationFlowActivity.EXTRAS_INSTRUCTIONS_DOCUMENT_SECOND_SIDE, R.string.emas_scan_second_side);
@@ -155,6 +174,97 @@ public class EmasIDActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    class MyImageListener implements ImageListener {
+
+        /**
+         * Called when library has image available.
+         */
+        @Override
+        public void onImageAvailable(Image image) {
+            // we will save images to 'myImages' folder on external storage
+            // image filenames will be 'imageType - currentTimestamp.jpg'
+            String output = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myImages";
+            File f = new File(output);
+            if(!f.exists()) {
+                f.mkdirs();
+            }
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+            String dateString = dateFormat.format(new java.util.Date());
+            String filename = null;
+            switch(image.getImageFormat()) {
+                case ALPHA_8: {
+                    filename = output + "/alpha_8 - " + image.getImageName() + " - " + dateString + ".jpg";
+                    break;
+                }
+                case BGRA_8888: {
+                    filename = output + "/bgra - " + image.getImageName() + " - " + dateString + ".jpg";
+                    break;
+                }
+                case YUV_NV21: {
+                    filename = output + "/yuv - " + image.getImageName()+ " - " + dateString + ".jpg";
+                    break;
+                }
+            }
+            Bitmap b = image.convertToBitmap();
+            Uri faceUri = Uri.fromFile(f);
+            Log.e("Image Listener", faceUri.toString());
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(filename);
+                boolean success = b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                if(!success) {
+                    Log.e("EMAS ImageListener", "Failed to compress bitmap!");
+                    if(fos != null) {
+                        try {
+                            fos.close();
+                        } catch (IOException ignored) {
+                        } finally {
+                            fos = null;
+                        }
+                        new File(filename).delete();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                Log.e("EMAS ImageListener", "Failed to save image");
+            } finally {
+                if(fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException ignored) {
+                    }
+                }
+            }
+            // after this line, image gets disposed. If you want to save it
+            // for later, you need to clone it with image.clone()
+        }
+
+        /**
+         * ImageListener interface extends Parcelable interface, so we also need to implement
+         * that interface. The implementation of Parcelable interface is below this line.
+         */
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+        }
+
+        public static final Creator<MyImageListener> CREATOR = new Creator<MyImageListener>() {
+            @Override
+            public MyImageListener createFromParcel(Parcel source) {
+                return new MyImageListener();
+            }
+
+            @Override
+            public MyImageListener[] newArray(int size) {
+                return new MyImageListener[size];
+            }
+        };
     }
 
 }
