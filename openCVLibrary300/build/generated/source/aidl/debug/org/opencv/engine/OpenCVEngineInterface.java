f2029db90808dbfc6032d19e5fab4d5f85c31f6e
Cheria <widayanto_cheria@mymail.sutd.edu.sg>
2018-03-31 15:35:12 +0800

    Added new drawable images

--- openCVLibrary300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,5 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Jing Yun\\Desktop\\50.003 Software Construction\\KYC\\Kodyac\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  * Original file: C:\\Users\\CHERIA\\Downloads\\Kodyac\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
